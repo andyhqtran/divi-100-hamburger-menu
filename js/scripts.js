e0ed08900ffc7fa574809c79cdba9fe5d511300e
Andy Tran <hello@andytran.me>
2016-01-19 17:21:59 -0800

    Added in accent color check.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,27 @@
 jQuery(document).ready(function ($) {
 
-  // Appends current search to body
-  // Replaces .et_search_outer with .et_custom_search
-  // Removes unnessary div
+  // Finds .mobile_menu_bar_toggle class
+  // Adds .et_divi_100_custom_hamburger_menu class
+  // Add 3 divs to html
+
   $(".mobile_menu_bar_toggle")
     .addClass("et_divi_100_custom_hamburger_menu")
     .html('<div></div><div></div><div></div>');
 
+  // Gets theme Accent Color
+  var accentColor = $("a").css("color");
 
+  // Append accent color style block
+  $('<style> .et_divi_100_custom_hamburger_menu div {background: ' + accentColor + '} .et_divi_100_custom_hamburger_menu--type-2 .et_divi_100_custom_hamburger_menu div, .et_divi_100_custom_hamburger_menu--type-3 .et_divi_100_custom_hamburger_menu div {background: 0; } .et_divi_100_custom_hamburger_menu--type-2 .et_divi_100_custom_hamburger_menu div:before, .et_divi_100_custom_hamburger_menu--type-2 .et_divi_100_custom_hamburger_menu div:after, .et_divi_100_custom_hamburger_menu--type-3 .et_divi_100_custom_hamburger_menu div:before, .et_divi_100_custom_hamburger_menu--type-3 .et_divi_100_custom_hamburger_menu div:after {background: ' + accentColor + '} </style>').appendTo('body');
+
+  // Menu click event
   $('.et_divi_100_custom_hamburger_menu').on('click', function (e) {
     e.preventDefault();
     $(this).toggleClass('et_divi_100_custom_hamburger_menu--toggled');
-    return console.log('Success: ' + $(this).attr('class'));
   });
+
   $('.toggle_all').on('click', function (e) {
     e.preventDefault();
     $('.et_divi_100_custom_hamburger_menu').toggleClass('et_divi_100_custom_hamburger_menu--toggled');
-    return console.log('Successful');
   });
 });
