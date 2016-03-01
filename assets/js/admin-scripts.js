4663003d702dae35717e585be38430d58e5fa6c0
Fikri Rasyid <fikrirasyid@gmail.com>
2016-03-01 21:05:46 +0700

    Updated submodule ref (use ePanel style settings page)

--- assets/js/admin-scripts.js
+++ assets/js/admin-scripts.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function ($) {
-	var $form_table = $('.et-divi-100-form-table'),
+	var $form_table = $('.et-divi-100-form'),
 		$menu_type  = $('#et_divi_100_custom_hamburger_menu-type'),
 		$menu_style = $('#et_divi_100_custom_hamburger_menu-style');
 
 	// Prepend preview container
-	$form_table.find('tr:last td:last').append( $( '<div />', { id : 'hamburger-menu-preview' }) );
+	$form_table.find('.epanel-box:last .box-content').append( $( '<div />', { id : 'hamburger-menu-preview' }) );
 
 	// Update preview
 	function update_hamburger_menu_preview() {
@@ -27,4 +27,37 @@ jQuery(document).ready(function ($) {
 	$form_table.on( 'change', 'select', function() {
 		update_hamburger_menu_preview();
 	});
+
+	// Top button
+	$('#epanel-save-top').click(function(e){
+		e.preventDefault();
+
+		$('#epanel-save').trigger('click');
+	});
+
+	// Help box
+	$(".box-description").click(function(){
+		var descheading = $(this).parent('.epanel-box').find(".box-title h3").html();
+		var desctext = $(this).parent('.epanel-box').find(".box-title .box-descr").html();
+
+		$('body').append("<div id='custom-lbox'><div class='box-desc'><div class='box-desc-top'>"+ et_divi_100_js_params.help_label +"</div><div class='box-desc-content'><h3>"+descheading+"</h3>"+desctext+"<div class='lightboxclose'></div> </div> <div class='box-desc-bottom'></div>	</div></div>");
+
+		$( '.lightboxclose' ).click( function() {
+			et_pb_close_modal( $( '#custom-lbox' ) );
+		});
+	});
+
+	function et_pb_close_modal( $overlay, no_overlay_remove ) {
+		var $modal_container = $overlay;
+
+		// add class to apply the closing animation to modal
+		$modal_container.addClass( 'et_pb_modal_closing' );
+
+		//remove the modal with overlay when animation complete
+		setTimeout( function() {
+			if ( 'no_remove' !== no_overlay_remove ) {
+				$modal_container.remove();
+			}
+		}, 600 );
+	}
 });
